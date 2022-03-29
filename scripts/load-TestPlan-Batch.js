0eb4282ac4c75cb863945ddd82787ca81c01322f
Quinta <marcos.quintanilha@mercadolivre.com>
2022-03-29 14:03:53 -0300

    script with request as an array

---
+++ scripts/load-TestPlan-Batch.js
@@ -0,0 +1,43 @@
+import http from 'k6/http'
+import { check, sleep } from 'k6'
+
+const BASE_URL = 'http://app-test.producer.melifrontends.com';
+const FURY_TOKEN = ''
+
+export const options = {
+    stages: [
+        // ramp-up from 1 to 5 VUs in 5s
+        { duration: '5s', target: 5 },
+
+        // stay at rest on 5 VUs for 30s
+        { duration: '30s', target: 5 },
+
+        // ramp-down from 5 to 0 VUs in 5s
+        { duration: '5s', target: 0 }
+    ],
+    thresholds: {
+        // throws error if more than 90% of the requests takes more than 2 seconds to be completed
+        http_req_duration: [
+            {
+                threshold: 'p(90) < 15000',
+                abortOnFail: true,
+                delayAbortEval: 100
+            }
+        ]
+    }
+}
+
+export default function() {
+    const params = {
+        headers: {
+          'x-auth-token': `${FURY_TOKEN}`,
+        },
+      };
+     
+    const responses = http.batch([
+        ['POST', `${BASE_URL}/dummy`, null, params],
+        ['GET', `${BASE_URL}/get_key`, null ,params],
+    ]);
+    check(responses[0], { "status is 200": (r) => r.status === 200 })
+    sleep(.300)
+}
